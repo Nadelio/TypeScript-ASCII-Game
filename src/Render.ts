795989f89d943d68b29e9faf36cde43e1952ff25
Nadelio <carefulsniffle@gmail.com>
2024-07-24 11:45:41 -0500

    Working...

--- src/Render.ts
+++ src/Render.ts
@@ -272,6 +272,3 @@ start();
 //TO/DO: enemy creation -> movement/ai -> collision detection -> dealing damage/taking damage
 //TO/DO: enemy health -> player/enemy attack -> enemy death
 //TODO: UI -> score system -> high score
-Render.js:11 Uncaught ReferenceError: exports is not defined
-at Render.js:11:23
-(anonymous) @ Render.js:11
